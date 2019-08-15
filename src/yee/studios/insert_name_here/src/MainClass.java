687095f6248e28a60fe3ae8457083dc7f479cc4e
InvalidName662 <54070204+InvalidName662@users.noreply.github.com>
2019-08-15 14:04:23 +0200

    Update Main.class
    Input Implementation (Wieder zu faul zum pushen)

--- src/yee/studios/insert_name_here/src/MainClass.java
+++ src/yee/studios/insert_name_here/src/MainClass.java
@@ -1,11 +1,14 @@
 package yee.studios.insert_name_here.src;
 
 import processing.core.PApplet;
+import java.awt.KeyboardFocusManager;
+import yee.studios.zeldagame.util.Input;
 
 public class MainClass extends PApplet{
 	
 	public static void main(String[] args) {
 		//Insert the full class path of this
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new Input());
 		PApplet.main("yee.studios.insert_name_here.src.MainClass");
 	}
 
